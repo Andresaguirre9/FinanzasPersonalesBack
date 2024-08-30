340b82a9c577900b544dbc7bda0c89594bd5eaa2
Andres20487 <andresag1220@gmail.com>
2024-08-29 20:43:04 -0500

    Conexión con la base de datos

---
+++ api/models/Login.js
@@ -0,0 +1,43 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  datastore: 'finanzaspersonalesdb',
+
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    password: {
+      type: 'string',
+      protect: true,
+      required: true,
+      description: 'Representación hash de la clase de un identificacion.',
+      example: '2$28a8eabna301089103-13948134nad',
+    },
+    usuario: {
+      type: 'string',
+      unique: true,
+      isEmail: true,
+      maxLength: 80,
+      required: true,
+      example: 'mary.escobar@example.com',
+      extendedDescription: 'Email ',
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+  customToJSON: function () {
+    return _.omit(this, [
+    ]);
+  },
+};
