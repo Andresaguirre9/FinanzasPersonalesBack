02d0ce319892f38fc5111980113f047acd2c0281
JuanLema14 <andres.lema1@udea.edu.co>
2024-10-14 20:02:53 -0500

    Controller de consulta de cuentas

---
+++ api/controllers/gestion-cuentas/consultar-cuenta.js
@@ -0,0 +1,51 @@
+module.exports = {
+  friendlyName: 'Consultar cuenta',
+
+  description: 'Consultar una cuenta en la BD',
+
+  inputs: {
+    idCuenta: {
+      type: 'number',
+      required: true,
+    },
+  },
+
+  exits: {},
+
+  fn: async function ({ idCuenta }) {
+    sails.log.verbose('-----> Consultar cuenta');
+
+    try {
+
+      const usuarioLogueado = this.req.decoded.sub.id
+
+      const cuentaConsultada = await Cuentas.findOne({
+        id_login: usuarioLogueado,
+        id: idCuenta
+      })
+
+      sails.log.verbose('Cuenta encontrada en la BD', cuentaConsultada);
+
+      return {
+        ejecucion: {
+          respuesta: {
+            estado: 'OK',
+            message: 'La cuenta se encontro con exito',
+          },
+          datos: { cuentaConsultada },
+        },
+      };
+    } catch (error) {
+      sails.log.error('cuentas', error);
+      return {
+        ejecucion: {
+          respuesta: {
+            estado: 'NOK',
+            message: error.message,
+          },
+          datos: {},
+        },
+      };
+    }
+  },
+};
