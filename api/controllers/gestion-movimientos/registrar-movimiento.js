21863eb8cf9e2335eddca879ad02af7665745799
JuanLema14 <andres.lema1@udea.edu.co>
2024-10-29 22:21:04 -0500

    funcional

--- api/controllers/gestion-movimientos/registrar-movimiento.js
+++ api/controllers/gestion-movimientos/registrar-movimiento.js
@@ -29,14 +29,15 @@ module.exports = {
       }
 
       const cuenta = validacionCuenta.ejecucion.datos.cuentaConsultada;
-      datosMovimiento.valor_movimiento = parseFloat(datosMovimiento.valor_movimiento);
+      sails.log.verbose("Cuenta antes de las modificaciones", cuenta);
+      datosMovimiento.valor_movimiento = parseInt(datosMovimiento.valor_movimiento);
 
 
       // Actualizar saldo según el tipo de movimiento
       if (datosMovimiento.tipo_movimiento === "ingreso") {
-        cuenta.saldo_actual = parseFloat(cuenta.saldo_actual) + datosMovimiento.valor_movimiento;
+        cuenta.saldo_actual += datosMovimiento.valor_movimiento;
       } else if (datosMovimiento.tipo_movimiento === "egreso") {
-        cuenta.saldo_actual = parseFloat(cuenta.saldo_actual) - datosMovimiento.valor_movimiento;
+        cuenta.saldo_actual -= datosMovimiento.valor_movimiento;
 
         if (cuenta.saldo_actual < 0) {
           throw new Error("Saldo insuficiente para realizar este movimiento.");
@@ -48,12 +49,18 @@ module.exports = {
       // Registrar la fecha de transacción
       cuenta.fecha_transaccion = new Date();
 
-      // Actualizar la cuenta en la BD
-      await Cuentas.updateOne({ id: datosMovimiento.id_cuenta }).set(cuenta);
+
+      sails.log("Modificacion de cuenta: ", cuenta)
+
+      await Cuentas.updateOne({ id: cuenta.id }).set({
+        saldo_actual: cuenta.saldo_actual,
+        fecha_transaccion: cuenta.fecha_transaccion
+      });
 
       // Registrar el movimiento en una transacción para mayor seguridad
       let registroMovimiento;
       await Movimientos.getDatastore().transaction(async (db) => {
+        sails.log("registro de movimiento: ", datosMovimiento)
         registroMovimiento = await Movimientos.create(datosMovimiento)
           .usingConnection(db)
           .fetch();
